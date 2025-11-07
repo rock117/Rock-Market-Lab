a16bccbb8a03c42618abd185e1cfa902b08a044e
rock huang <manj22@163.com>
2025-11-07 09:28:10 +0800

    tushare 使用 tushare_api crate获取数据

--- api/entity/gen-entity.py
+++ api/entity/gen-entity.py
@@ -1,6 +1,6 @@
 import os
 os.chdir('C:/rock/coding/code/my/rust/Rock-Market-Lab/api/entity')
-os.system('sea-orm-cli generate entity  -u postgres://postgres:123456@localhost:5432/investment_research  -o src --expanded-format  --lib --with-serde both')
+os.system('sea-orm-cli generate entity  -u mysql://root:123456@localhost:3306/investment_research  -o src --expanded-format  --lib --with-serde both')
 print('gen entity complete')
 
 
