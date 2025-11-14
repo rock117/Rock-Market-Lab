726ed3b9358fc7b38cb930f7cbbb89ddf1e90183
rock huang <manj22@163.com>
2025-11-14 18:15:22 +0800

    remove unused

--- api/service/src/analysis/mod.rs
+++ api/service/src/analysis/mod.rs
@@ -1,10 +1,1 @@
 mod limit_up_down;
-pub mod technical_stock_picker;
-pub mod stock_data_provider;
-pub mod stock_picking_service;
-pub mod examples;
-
-// 重新导出主要类型
-pub use technical_stock_picker::{TechnicalStockPicker, TechnicalAnalysisResult, StockPickingCriteria};
-pub use stock_data_provider::{StockDataProvider, DataProviderConfig};
-pub use stock_picking_service::{StockPickingService, StockPickingRequest, StockPickingResponse, StockPickingStrategy};
