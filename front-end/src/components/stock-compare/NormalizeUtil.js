d2fa1eb097bad12ab26711ec7c3e3014cb2c87b8
rock huang <manj22@163.com>
2025-12-08 19:16:00 +0800

    remove old front end

--- front-end/src/components/stock-compare/NormalizeUtil.js
+++
@@ -1,9 +0,0 @@
-const normalize = prices => {
-    let basePrice = prices[0] // 以第一个价格为基准
-    // return prices.map(price => ((price - basePrice) / basePrice) * 100)
-    return prices
-}
-
-export {
-    normalize
-}
--- front-end/src/util/index.js
+++
@@ -1,6 +0,0 @@
-import * as http from "./http";
-
-
-export {
-    http
-}
